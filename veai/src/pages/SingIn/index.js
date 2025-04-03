138fd6c6e9afd6d62026f3e9418f3a37f0510b8a
valter Neto <vtvaltervt@gmail.com>
2025-04-03 11:46:24 -0300

    tela de login pronta

--- veai/src/pages/SingIn/index.js
+++ veai/src/pages/SingIn/index.js
@@ -3,6 +3,7 @@ import './singin.css';
 import Camada_1 from '../../assets/Camada_1.png';
 import userimg from '../../assets/user.png';
 import chevron from '../../assets/chevron-left.png';
+import lock from '../../assets/lock.png';
 import { Link } from 'react-router-dom'; 
 
 import { AuthContext } from '../../contexts/Auth';
@@ -21,28 +22,38 @@ export default function SignIn(){
     }
 
     return(
-        <div className='conteiner-center' >
-            <div className='login-area' >
-                <div className='botão-voltar' >
-                    <Link to ="/"><img src={chevron} /></Link>
-                </div>
-                <img src={Camada_1} alt='logo da marca' />
-            </div>
-            <div className='login' >
-                <form onSubmit={handleSigIn} >
-                    <div className='input-img' >
-                    <img src={userimg} />
-                    <input type='text' placeholder='email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+        <div>
+        <div className="img_voltar">
+        <Link to ="/"><img src={chevron} classNameName= "voltar" /></Link>
+        </div>
+        <div>
+            <div className="modulo">
+                <div className="modulo_1">
+                    <div className="simbolo_login">
+                    <img src={Camada_1} alt='logo da marca' />
                     </div>
-                
-                    <input type='senha' placeholder='senha' value={password} onChange={(e)=> setPassword(e.target.value)}/>
-                    <button type='submit'> {loadingAuth ? "carregando..." : "Entrar"} </button>
-                </form>
-                <div className='link' >
-                Não tem conta?
-                <Link to="/singup" >Crie uma conta</Link>
+                    <div className="texto_login">
+                        <p>Uma comunidade confiável e intuitiva para as negociações estudantis.</p>
+                    </div>
+                </div>
+                <div className="bloco">
+                    <form onSubmit={handleSigIn} >
+                    <div className="bloco_1">
+                        <img src={userimg} />
+                        <input type='text' placeholder='email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                    </div>
+                    <div className="bloco_2">
+                        <img src={userimg} />
+                        <input type='senha' placeholder='senha' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+                    </div>
+                    <button type='submit'> {loadingAuth ? "carregando..." : "acessar"} </button>
+                    </form>
+                    <div className="Grup_text">
+                        Não tem conta? <Link to="/singup" className="link" >Crie uma conta</Link>
+                    </div>
                 </div>
             </div>
+        </div>
         </div>
     )
 }
