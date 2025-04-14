3e7fea8bea84473dcab17930ee41509528dbd24c
valter Neto <vtvaltervt@gmail.com>
2025-04-14 14:21:48 -0300

    ajuste do bloco inicial e do bloco de anuncios da home

--- veai/src/pages/Home/index.js
+++ veai/src/pages/Home/index.js
@@ -1,19 +1,59 @@
 import { Link } from "react-router-dom";
-import Header from "../../components/Header";
+import bookmark from '../../assets/bookmark.png';
+import iconsearch from '../../assets/iconsearch.png';
+import './home.css';
 export default function Home(){
     return(
-        <div>
-            <Header/>
-            <h1>Página inicial</h1>
-            <Link to="/singin">
-            <button> Login </button>
-            </Link>
-            <Link to="/singup">
-            <button> Cadastro </button>
-            </Link>
+        
+        <div className="area">
+            <div className="bloco_cabeçalho">
+                <img src={bookmark} />
+                <button className="botao_anuncio">Anunciar</button>
+                <div className="area_login">
+                    
+                    <Link to="/singin"> Login </Link>
+                
+                </div>
+            </div>
             <Link to="/singnupprodcts">
             <button> Cadastro de produtos </button>
             </Link>
+            <div className="bloco_guias" >
+                <div className="bloco_guia_texto" >
+                    Uma comunidade confiável e intuitiva para as negociaçõesestudantis.
+                </div>
+               <div className="bloco_guias_bloco2">
+                    <div className="bloco_guias_bloco2_pesquisa" >
+                        <div className="bloco_guias_bloco2_pesquisa2">
+                            <img src={iconsearch} className="bloco_guias_bloco2_img_pesquisa"/>
+                            <input type="text" placeholder="vê aí se tem ..."/>
+                        </div>
+                    </div>
+                    <div className="bloco_guias_bloco2_botoes">
+                        <div className="bloco_guias_bloco2_botoes2">
+                            <button>Livros</button>
+                            <button>Eletrônicos</button>
+                            <button>Móveis</button>
+                        </div>
+                    </div>
+               </div> 
+               <div className="bloco_guias_bloco2_anuncio">
+                    <div className="bloco_guias_bloco2_anuncio2">
+                        anúncios
+                    </div>
+                    <div className="bloco_guias_bloco2_anuncio3">
+                        anúncios
+                    </div>
+               </div>
+            </div>
+            <div>
+                <div>
+                    anúncios em destaque
+                </div>
+                <div>
+                    móveis
+                </div>
+            </div>
         </div>
     )
 }
